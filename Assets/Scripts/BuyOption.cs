6ae8f5d61a8995a332707ef0e782213d2a30715a
Oskari Liukku <oliukku@gmail.com>
2017-07-29 17:34:36 +0300

    buying items

--- Assets/Scripts/BuyOption.cs
+++ Assets/Scripts/BuyOption.cs
@@ -9,29 +9,45 @@ public class BuyOption : MonoBehaviour
     private BuyOptionData _optionData;
     [SerializeField] private TextMeshProUGUI _titleText;
     [SerializeField] private TextMeshProUGUI _costText;
+    private int _ownedAmount = 0;
+    [SerializeField] private TextMeshProUGUI _ownedAmountText;
     [SerializeField] private Image _iconImage;
 
     public void Initialize(BuyOptionData data)
     {
         _optionData = data;
         SetTitle(_optionData.Name);
-        SetCost(_optionData.GetCostOfLevel(1));
-        print(_optionData.GetCostOfLevel(1) + " " + _optionData.CostMultiplierPerLevel + " " + _optionData.StartingCost);
+        SetCost(_optionData.StartingCost);
         SetIcon(_optionData.Icon);
     }
 
-    public void SetTitle(string newTitle)
+    private void SetTitle(string newTitle)
     {
         _titleText.text = newTitle;
     }
 
-    public void SetCost(long newCost)
+    private void SetCost(long newCost)
     {
         _costText.text = newCost.ToString();
     }
 
-    public void SetIcon(Sprite newIcon)
+    private void SetOwnedAmount(int newAmount)
+    {
+        _ownedAmountText.text = newAmount.ToString();
+    }
+
+    private void SetIcon(Sprite newIcon)
     {
         _iconImage.sprite = newIcon;
+    }
+
+    public void Buy(int amount)
+    {
+        // Remove money
+        // Increase income
+
+        _ownedAmount = _ownedAmount + amount;
+        SetOwnedAmount(_ownedAmount);
+        SetCost(_optionData.GetCostOfLevel(_ownedAmount + 1));
     }
 }
