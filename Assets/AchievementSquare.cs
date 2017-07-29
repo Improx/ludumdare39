4d981a515d3cc2291d7d8c4795ac7338f7bc0a1c
Miguelosss <heckmiika@hotmail.com>
2017-07-29 11:52:16 +0300

    achivement squares

---
+++ Assets/AchievementSquare.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementSquare : MonoBehaviour
+{
+
+    [SerializeField] private Image _icon;
+    private string _title;
+    private string _description;
+
+    public void Initialize(Sprite icon, string title, string description)
+    {
+        _icon.sprite = icon;
+        _title = title;
+        _description = description;
+    }
+}
